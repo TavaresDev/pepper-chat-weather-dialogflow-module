da9e3c4c6b26ce799132bd5d35e1270f86629a13
DanieltheYogi <daniel.lane.schofield@gmail.com>
2019-10-07 16:51:21 -0700

    added more logging

--- weather.js
+++ weather.js
@@ -55,6 +55,7 @@ class WeatherPepperChat {
                     timeContext = 'future';
                 }
             }
+            console.log(api_response)
             const response_body = api_response.body(false);
             const forecast = path(['data', 'weather', 0], response_body);
             const location = path(['data', 'request', 0], response_body);
